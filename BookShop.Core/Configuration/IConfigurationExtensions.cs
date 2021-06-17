31f96cc01cd770e9c888d4d6347a57ebb5ae6386
Danielius <danielius.vl@gmail.com>
2021-06-17 23:33:09 +0300

    Added internal class with extension method IsDevelopment

---
+++ BookShop.Core/Configuration/IConfigurationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace BookShop.Core.Configuration
+{
+    internal static class IConfigurationExtensions
+    {
+        private const string enviroment = "ASPNETCORE_ENVIRONMENT";
+
+        private const string development = "Development";
+
+        internal static bool IsDevelopment(this IConfiguration configuration)
+        {
+            return configuration.GetValue<string>(enviroment) == development;
+        }
+    }
+}
