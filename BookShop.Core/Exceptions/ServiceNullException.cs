5021344370a92ba544955336751d98103d85cf2e
Danielius <danielius.vl@gmail.com>
2021-06-17 23:34:22 +0300

    Added ServiceNullExceptions class

---
+++ BookShop.Core/Exceptions/ServiceNullException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BookShop.Core.Exceptions
+{
+    public class ServiceNullException : Exception
+    {
+        public ServiceNullException(string paramName, string where)
+        {
+            ParamName = paramName;
+            Where = where;
+        }
+
+        public string ParamName { get; }
+
+        public string Where { get; }
+    }
+}
