a3ce03991d6e83de1f9f91e0c9dec123887baa2c
Danielius <danielius.vl@gmail.com>
2021-06-18 01:10:19 +0300

    Added AutoMapper Profile class

---
+++ BookShop.Core/AutoMapperProfiles/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BookShop.Core.Models;
+using BookShop.Domain.Entities;
+
+namespace BookShop.Core.AutoMapperProfiles
+{
+    class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<BookAuthor, BookAuthorModel>().ReverseMap();
+        }
+    }
+}
