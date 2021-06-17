4848ff1565ce3f0db04b99fe963edb7f3ed9089d
Danielius <danielius.vl@gmail.com>
2021-06-17 21:53:56 +0300

    Renamed DateAdded property to DateReleased

--- BookShop.Domain/Entities/Product.cs
+++ BookShop.Domain/Entities/Product.cs
@@ -16,7 +16,7 @@ namespace BookShop.Domain.Entities
 
         public bool Hidden { get; set; }
 
-        public DateTime DateAdded { get; set; } = DateTime.Now;
+        public DateTime DateReleased { get; set; } = DateTime.Now;
 
         public IList<BookPhoto> Photos { get; set; } = new List<BookPhoto>();
 
