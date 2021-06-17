7536ff89451b3cc9fb0bfd30f32fa1816826f194
Danielius <danielius.vl@gmail.com>
2021-06-18 01:15:12 +0300

    Added delete, update, and getbyid query

---
+++ BookShop.Core/Mediatr/BookAuthor/Commands/Update/UpdateBookAuthor.cs
@@ -0,0 +1,60 @@
+﻿using BookShop.Core.Abstract.Repositories;
+using BookShop.Core.Configuration;
+using BookShop.Core.Exceptions;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookShop.Core.Mediatr.BookAuthor.Commands.Update
+{
+    public static class UpdateBookAuthor
+    {
+        public record Command(Domain.Entities.BookAuthor BookAuthor) : IRequest;
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly IBookAuthorRepository repository;
+            private readonly IConfiguration configuration;
+
+            public Handler(IBookAuthorRepository repository, IConfiguration configuration)
+            {
+                this.repository = repository;
+                this.configuration = configuration;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                if (repository == null)
+                {
+                    throw new ServiceNullException(nameof(IProductRepository), nameof(Handler));
+                }
+
+                if (configuration == null)
+                {
+                    throw new ServiceNullException(nameof(IConfiguration), nameof(Handler));
+                }
+
+                if (configuration.IsDevelopment())
+                {
+                    if (request == null)
+                    {
+                        throw new ArgumentNullException(nameof(request));
+                    }
+
+                    if (request.BookAuthor == null)
+                    {
+                        throw new ArgumentNullException(nameof(request.BookAuthor));
+                    }
+                }
+
+                // TODO: Add validation and logging.
+
+                await repository.Update(request?.BookAuthor);
+
+                return default;
+            }
+        }
+    }
+}
---
+++ BookShop.Core/Mediatr/BookAuthor/Queries/GetById/GetByIdBookAuthor.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using BookShop.Core.Abstract.Repositories;
+using BookShop.Core.Configuration;
+using BookShop.Core.Exceptions;
+using BookShop.Core.Models;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BookShop.Core.Mediatr.BookAuthor.Queries.GetById
+{
+    public static class GetByIdBookAuthor
+    {
+        public record Query(int id) : IRequest<BookAuthorModel>;
+
+        public class Handler : IRequestHandler<Query, BookAuthorModel>
+        {
+            private readonly IBookAuthorRepository repository;
+            private readonly IConfiguration configuration;
+            private readonly IMapper mapper;
+
+            public Handler(IBookAuthorRepository repository, IConfiguration configuration, IMapper mapper)
+            {
+                this.repository = repository;
+                this.configuration = configuration;
+                this.mapper = mapper;
+            }
+
+            public async Task<BookAuthorModel> Handle(Query request, CancellationToken cancellationToken)
+            {
+                if (repository == null)
+                {
+                    throw new ServiceNullException(nameof(IProductRepository), nameof(Handler));
+                }
+
+                if (configuration == null)
+                {
+                    throw new ServiceNullException(nameof(IConfiguration), nameof(Handler));
+                }
+
+                if (configuration.IsDevelopment())
+                {
+                    if (request == null)
+                    {
+                        throw new ArgumentNullException(nameof(request));
+                    }
+                }
+
+                // TODO: Add validation and logging.
+
+                Domain.Entities.BookAuthor entity = await repository.GetById(request.id);
+
+                if(entity == null)
+                {
+                    throw new NotFoundException(nameof(Domain.Entities.BookAuthor), request.id);
+                }
+
+                BookAuthorModel model = mapper.Map<Domain.Entities.BookAuthor, BookAuthorModel>(entity);
+
+                return model;
+            }
+        }
+    }
+}
