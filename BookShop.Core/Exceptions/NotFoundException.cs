00220e01b6d881b222c8b7d27b1a6ef049b3aa2d
Danielius <danielius.vl@gmail.com>
2021-06-18 01:02:11 +0300

    Added NotFoundException class

---
+++ BookShop.Core/Exceptions/NotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BookShop.Core.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string paramName, int id)
+        {
+            ParamName = paramName;
+            Id = id;
+        }
+
+        public string ParamName { get; }
+        public int Id { get; }
+    }
+}
