e257d12f9d3d57c4a03b596ca3a0e62841693e4d
Danielius <danielius.vl@gmail.com>
2021-06-18 22:15:31 +0300

    Created BookPhotoModel

---
+++ BookShop.Core/Models/BookPhotoModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace BookShop.Core.Models
+{
+    public class BookPhotoModel
+    {
+        public int Id { get; set; }
+
+        public string FilePath { get; set; }
+
+        public IList<byte> FileBytes { get; set; }
+
+        public override string ToString()
+        {
+            return FilePath;
+        }
+    }
+}
