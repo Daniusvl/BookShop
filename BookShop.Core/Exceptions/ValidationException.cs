cbd34a6b4384e1a2a4b02e08549ad0ef19c19745
Danielius <danielius.vl@gmail.com>
2021-06-19 23:09:37 +0300

    Added new ctor with params string[] error_mesagges

--- BookShop.Core/Exceptions/ValidationException.cs
+++ BookShop.Core/Exceptions/ValidationException.cs
@@ -6,7 +6,7 @@ namespace BookShop.Core.Exceptions
 {
     public class ValidationException : Exception
     {
-        public IList<string> Errors { get; private set; }
+        public List<string> Errors { get; private set; }
 
         public ValidationException(ValidationResult result)
         {
@@ -15,6 +15,15 @@ namespace BookShop.Core.Exceptions
             foreach (ValidationFailure error in result.Errors)
             {
                 Errors.Add(error.ErrorMessage);
+            }
+        }
+
+        public ValidationException(params string[] error_messages)
+        {
+            Errors = new List<string>();
+            if(error_messages != null && error_messages.Length > 0)
+            {
+                Errors.AddRange(error_messages);
             }
         }
     }
