8d2181457160b78cead73c8ec345c931d8e68039
Danielius <danielius.vl@gmail.com>
2021-06-19 20:02:45 +0300

    Applied validation

--- BookShop.Core/Mediatr/BookPhoto/Commands/Delete/DeleteBookPhoto.cs
+++ BookShop.Core/Mediatr/BookPhoto/Commands/Delete/DeleteBookPhoto.cs
@@ -1,6 +1,7 @@
 ﻿using BookShop.Core.Abstract.Repositories;
 using BookShop.Core.Configuration;
 using BookShop.Core.Exceptions;
+using FluentValidation.Results;
 using MediatR;
 using Microsoft.Extensions.Configuration;
 using System;
@@ -17,12 +18,10 @@ namespace BookShop.Core.Mediatr.BookPhoto.Commands.Delete
         public class Handler : IRequestHandler<Command>
         {
             private readonly IBookPhotoRepository repository;
-            private readonly IConfiguration configuration;
 
-            public Handler(IBookPhotoRepository repository, IConfiguration configuration)
+            public Handler(IBookPhotoRepository repository)
             {
                 this.repository = repository;
-                this.configuration = configuration;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
@@ -32,17 +31,12 @@ namespace BookShop.Core.Mediatr.BookPhoto.Commands.Delete
                     throw new ServiceNullException(nameof(IBookPhotoRepository), nameof(Handler));
                 }
 
-                if (configuration == null)
-                {
-                    throw new ServiceNullException(nameof(IConfiguration), nameof(Handler));
-                }
+                RequestValidator validator = new();
+                ValidationResult result = await validator.ValidateAsync(request);
 
-                if (configuration.IsDevelopment())
+                if(result.Errors.Count > 0)
                 {
-                    if (request == null)
-                    {
-                        throw new ArgumentNullException(nameof(request));
-                    }
+                    throw new ValidationException(result);
                 }
 
                 // TODO: Add validation and logging.
