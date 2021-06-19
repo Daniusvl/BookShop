401f776c6cf32f3d1b17d510730f74c57834e86b
Danielius <danielius.vl@gmail.com>
2021-06-19 19:59:09 +0300

    Added validator for BookPhoto delete mediatr

---
+++ BookShop.Core/Mediatr/BookPhoto/Commands/Delete/RequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace BookShop.Core.Mediatr.BookPhoto.Commands.Delete
+{
+    public class RequestValidator : AbstractValidator<DeleteBookPhoto.Command>
+    {
+        public RequestValidator()
+        {
+            RuleFor(command => command)
+                .NotNull()
+                    .WithMessage("{PropertyName} cannot be null");
+        }
+    }
+}
