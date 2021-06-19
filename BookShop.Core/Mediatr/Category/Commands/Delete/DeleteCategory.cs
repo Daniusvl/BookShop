772caa84e71e17df46a73218ee505604167048e0
Danielius <danielius.vl@gmail.com>
2021-06-19 20:40:35 +0300

    Removed IConfiguration and applied validation

--- BookShop.Core/Mediatr/Category/Commands/Delete/DeleteCategory.cs
+++ BookShop.Core/Mediatr/Category/Commands/Delete/DeleteCategory.cs
@@ -1,12 +1,7 @@
 ﻿using BookShop.Core.Abstract.Repositories;
-using BookShop.Core.Configuration;
 using BookShop.Core.Exceptions;
+using FluentValidation.Results;
 using MediatR;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -19,12 +14,10 @@ namespace BookShop.Core.Mediatr.Category.Commands.Delete
         public class Handler : IRequestHandler<Command>
         {
             private readonly ICategoryRepository repository;
-            private readonly IConfiguration configuration;
 
-            public Handler(ICategoryRepository repository, IConfiguration configuration)
+            public Handler(ICategoryRepository repository)
             {
                 this.repository = repository;
-                this.configuration = configuration;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
@@ -34,17 +27,12 @@ namespace BookShop.Core.Mediatr.Category.Commands.Delete
                     throw new ServiceNullException(nameof(ICategoryRepository), nameof(Handler));
                 }
 
-                if (configuration == null)
-                {
-                    throw new ServiceNullException(nameof(IConfiguration), nameof(Handler));
-                }
+                RequestValidator validator = new();
+                ValidationResult result = await validator.ValidateAsync(request);
 
-                if (configuration.IsDevelopment())
+                if(result.Errors.Count > 0)
                 {
-                    if (request == null)
-                    {
-                        throw new ArgumentNullException(nameof(request));
-                    }
+                    throw new ValidationException(result);
                 }
 
                 // TODO: Add validation and logging.
