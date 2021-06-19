c48f6f3c747d50ce83515d65dc0082346ee7f85d
Danielius <danielius.vl@gmail.com>
2021-06-19 17:55:25 +0300

    Created validator for Create Handler

---
+++ BookShop.Core/Mediatr/BookAuthor/Commands/Create/RequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using BookShop.Core.Abstract.Repositories;
+using FluentValidation;
+
+namespace BookShop.Core.Mediatr.BookAuthor.Commands.Create
+{
+    internal class RequestValidator : AbstractValidator<string>
+    {
+        private readonly IBookAuthorRepository repository;
+
+        public RequestValidator(IBookAuthorRepository repository)
+        {
+            this.repository = repository;
+
+            RuleFor(s => s)
+                .NotNull()
+                    .WithMessage("{PropertyName} cannot be null")
+                .NotEmpty()
+                    .WithMessage("{PropertyName} cannot be empty")
+                .MaximumLength(200)
+                    .WithMessage("{PropertyName} cannot be more than 200 characters")
+                .Must(repository.IsUniqueName)
+                    .WithMessage("There is already author with this name: {PropertyName}");
+        }
+    }
+}
