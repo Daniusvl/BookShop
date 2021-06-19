13a57cc4c99286b53a17b307e1bbfbc62eb22442
Danielius <danielius.vl@gmail.com>
2021-06-19 18:00:19 +0300

    Created ValidationException

---
+++ BookShop.Core/Exceptions/ValidationException.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+
+namespace BookShop.Core.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public IList<string> Errors { get; private set; }
+
+        public ValidationException(ValidationResult result)
+        {
+            Errors = new List<string>();
+
+            foreach (ValidationFailure error in result.Errors)
+            {
+                Errors.Add(error.ErrorMessage);
+            }
+        }
+    }
+}
