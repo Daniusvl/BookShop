9d9d40765d3e351cab2707230c2595e21d1dc8c8
Danielius <danielius.vl@gmail.com>
2021-06-19 20:14:02 +0300

    Added validator for BookPhoto Update mediatr handler

---
+++ BookShop.Core/Mediatr/BookPhoto/Commands/Update/RequestValidator.cs
@@ -0,0 +1,27 @@
+﻿using BookShop.Core.Abstract.Repositories;
+using FluentValidation;
+
+namespace BookShop.Core.Mediatr.BookPhoto.Commands.Update
+{
+    internal class RequestValidator : AbstractValidator<UpdateBookPhoto.Command>
+    {
+        private readonly IBookPhotoRepository repository;
+
+        internal RequestValidator(IBookPhotoRepository repository)
+        {
+            this.repository = repository;
+
+            RuleFor(command => command)
+                .NotNull()
+                    .WithMessage("{PropertyName} cannot be null");
+
+            RuleFor(command => command.BookPhoto)
+                .NotNull()
+                    .WithMessage("{PropertyName} cannot be null");
+
+            RuleFor(command => command.BookPhoto.FilePath)
+                .Must(repository.PhotoExists)
+                    .WithMessage("Photo in this directory does not exist: {PropertyName}");
+        }
+    }
+}
