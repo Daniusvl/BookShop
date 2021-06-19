274afda309651cac3b856518195d47ad8ed45a09
Danielius <danielius.vl@gmail.com>
2021-06-19 20:39:16 +0300

    Added validator for DeleteCategory handler

---
+++ BookShop.Core/Mediatr/Category/Commands/Delete/RequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace BookShop.Core.Mediatr.Category.Commands.Delete
+{
+    internal class RequestValidator : AbstractValidator<DeleteCategory.Command>
+    {
+        internal RequestValidator()
+        {
+            RuleFor(command => command)
+                .NotNull()
+                    .WithMessage("{propertyName} cannot be null");
+        }
+    }
+}
