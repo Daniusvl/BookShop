2efb691c10456a15548d18f346e5d91160d4f17e
Danielius <danielius.vl@gmail.com>
2021-06-19 16:11:13 +0300

    Created service collection extensions

---
+++ BookShop.Core/IServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using BookShop.Core.AutoMapperProfiles;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BookShop.Core
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMediator(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+
+        public static IServiceCollection AddAutoMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(AutoMapperProfile));
+            return services;
+        }
+    }
+}
