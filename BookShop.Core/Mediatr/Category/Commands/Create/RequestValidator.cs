bb5ef60070a1792dd83277f3ce9090443975a2ce
Danielius <danielius.vl@gmail.com>
2021-06-19 20:32:29 +0300

    Added validator for CreateCategory handler

---
+++ BookShop.Core/Mediatr/Category/Commands/Create/RequestValidator.cs
@@ -0,0 +1,28 @@
+﻿using BookShop.Core.Abstract.Repositories;
+using FluentValidation;
+
+namespace BookShop.Core.Mediatr.Category.Commands.Create
+{
+    internal class RequestValidator : AbstractValidator<CreateCategory.Command>
+    {
+        private readonly ICategoryRepository repository;
+
+        internal RequestValidator(ICategoryRepository repository)
+        {
+            this.repository = repository;
+
+            RuleFor(command => command)
+                .NotNull()
+                    .WithMessage("{PropertyName} cannot be null");
+
+            RuleFor(command => command.Name)
+                .NotNull()
+                    .WithMessage("{PropertyName} cannot be null")
+                .NotEmpty()
+                    .WithMessage("{PropertyName} cannot be empty")
+                .Must(repository.IsUniqueName)
+                    .WithMessage("Category with specified name already exists: {PropertyName}");
+                
+        }
+    }
+}
