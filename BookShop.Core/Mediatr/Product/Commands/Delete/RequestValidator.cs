1ab57e6a1e3c61b998b69f181f156d913e8eaa77
Danielius <danielius.vl@gmail.com>
2021-06-19 21:06:09 +0300

    Added validator for DeleteProduct handler

---
+++ BookShop.Core/Mediatr/Product/Commands/Delete/RequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace BookShop.Core.Mediatr.Product.Commands.Delete
+{
+    internal class RequestValidator : AbstractValidator<DeleteProduct.Command>
+    {
+        internal RequestValidator()
+        {
+            RuleFor(command => command)
+                .NotNull()
+                    .WithMessage("{PropertyName} cannot be null");
+        }
+    }
+}
