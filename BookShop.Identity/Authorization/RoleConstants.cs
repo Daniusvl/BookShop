327e8f7cd5da35fce5448b8b12e08490f43d4801
Danielius <danielius.vl@gmail.com>
2021-06-20 21:52:04 +0300

    Installed packages, added authentication and authorization configuration

---
+++ BookShop.Identity/Authorization/RoleConstants.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace BookShop.Identity.Authorization
+{
+    public static class RoleConstants
+    {
+        public const string RoleClaim = "Role";
+
+        public static readonly IList<string> DefaultUser;
+        public const string DefaultUserName = "DefaultUser";
+
+        public static readonly IList<string> Moderator;
+        public const string ModeratorName = "Moderator";
+
+        public static readonly IList<string> Administrator;
+        public const string AdministratorName = "Administrator";
+
+        static RoleConstants()
+        {
+            DefaultUser = new List<string> { "DefaultUser", "Moderator", "Administrator" };
+            Moderator = new List<string> { "Moderator", "Administrator" };
+            Administrator = new List<string> { "Administrator" };
+        }
+    }
+}
---
+++ BookShop.Identity/IServiceCollectionExtensions.cs
@@ -0,0 +1,61 @@
+﻿using BookShop.Core.Configuration;
+using BookShop.Identity.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookShop.Identity
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAuthenticationAuthorization(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IdentityDb>(options =>
+            {
+                if (configuration.IsDevelopment())
+                {
+                    options.UseInMemoryDatabase("IdentityDb");
+                }
+                else
+                {
+                    options.UseSqlServer(configuration.GetConnectionString("IdentityDb"));
+                }
+            });
+
+            services.AddIdentityCore<IdentityUser>(options => 
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.User.RequireUniqueEmail = true;
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+            })
+                .AddEntityFrameworkStores<IdentityDb>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            services.AddAuthorization(cfg =>
+            {
+                cfg.AddPolicy(RoleConstants.DefaultUserName, policy_cfg =>
+                {
+                    policy_cfg.RequireClaim(RoleConstants.RoleClaim, RoleConstants.DefaultUser);
+                });
+
+                cfg.AddPolicy(RoleConstants.ModeratorName, policy_cfg =>
+                {
+                    policy_cfg.RequireClaim(RoleConstants.RoleClaim, RoleConstants.Moderator);
+                });
+
+                cfg.AddPolicy(RoleConstants.AdministratorName, policy_cfg =>
+                {
+                    policy_cfg.RequireClaim(RoleConstants.RoleClaim, RoleConstants.Administrator);
+                });
+
+            });
+
+            return services;
+        }
+    }
+}
