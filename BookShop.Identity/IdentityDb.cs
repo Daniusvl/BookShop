8616531cdca68739b4dd8c91bda1d30796b13c8b
Danielius <danielius.vl@gmail.com>
2021-06-20 19:43:15 +0300

    Added BookShop.Identity project

---
+++ BookShop.Identity/IdentityDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShop.Identity
+{
+    public class IdentityDb : IdentityDbContext<IdentityUser>
+    {
+        public IdentityDb(DbContextOptions<IdentityDb> options) : base(options)
+        {
+        }
+    }
+}
---
+++ BookShop.Identity/Services/AuthenticationService.cs
@@ -0,0 +1,20 @@
+﻿using BookShop.Core.Abstract.Identity;
+using BookShop.Core.Models.Authentication;
+using System;
+using System.Threading.Tasks;
+
+namespace BookShop.Identity.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        public Task<AuthenticationModel> Authenticate(AuthenticationRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RegisterModel> Register(RegisterRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
