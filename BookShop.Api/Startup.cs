3b67c7a3a236596d24d3a37ef19dda5e383ab92e
Danielius <danielius.vl@gmail.com>
2021-06-21 18:38:28 +0300

    Removed unused usings

--- BookShop.Api/Startup.cs
+++ BookShop.Api/Startup.cs
@@ -4,14 +4,8 @@ using BookShop.Identity;
 using BookShop.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BookShop.Api
 {
