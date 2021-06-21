4c93ecef51192a6ebb8629e0913ba18a9d84baed
Danielius <danielius.vl@gmail.com>
2021-06-21 19:22:42 +0300

    Added AuthController

---
+++ BookShop.Api/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using BookShop.Core.Abstract.Identity;
+using BookShop.Core.Models.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BookShop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthenticationService service;
+
+        public AuthController(IAuthenticationService service)
+        {
+            this.service = service;
+        }
+
+        [HttpPost("/Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            RegisterModel model = await service.Register(request);
+            if (!model.Success)
+            {
+                return BadRequest(model.PasswordErrors);
+            }
+            return Ok(model);
+        }
+
+        [HttpPost("/Authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] AuthenticationRequest request)
+        {
+            AuthenticationModel model = await service.Authenticate(request);
+            if (!model.Success)
+            {
+                return BadRequest();
+            }
+            return Ok(model);
+        }
+    }
+}
