a03953aede00d8b9f24d9a544869086cd513950d
Danielius <danielius.vl@gmail.com>
2021-06-21 19:00:55 +0300

    Removed unused usings

--- BookShop.Api/Controllers/BookPhotoController.cs
+++ BookShop.Api/Controllers/BookPhotoController.cs
@@ -6,11 +6,8 @@ using BookShop.Core.Models;
 using BookShop.Identity.Authorization;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookShop.Api.Controllers
