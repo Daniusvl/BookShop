39e36c012b510d0d7f2a576a109f86ab02d2ce42
Danielius <danielius.vl@gmail.com>
2021-06-21 16:18:08 +0300

    Installed Newtonsoft.Json and added AppUser that holds OwnedProducts in json

---
+++ BookShop.Identity/Models/AppUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace BookShop.Identity.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public AppUser(string userName) : base (userName){}
+
+        public string OwnedProducts { get; set; } = "[]";
+
+        public IList<OwnedProduct> GetOwnedProducts()
+        {
+            return JsonConvert.DeserializeObject<List<OwnedProduct>>(OwnedProducts) ?? new List<OwnedProduct>();
+        }
+
+        public void AddOwnedProduct(int id, string name, string file_path)
+        {
+            IList<OwnedProduct> products = GetOwnedProducts();
+            products.Add(new OwnedProduct { Id = id, Name = name, FilePath = file_path });
+            OwnedProducts = JsonConvert.SerializeObject(products);
+        }
+    }
+}
---
+++ BookShop.Identity/Models/OwnedProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShop.Identity.Models
+{
+    public class OwnedProduct
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string FilePath { get; set; }
+    }
+}
