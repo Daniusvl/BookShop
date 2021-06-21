df0ed5b9dea04960a1014abf4559eefd3dcc0778
Danielius <danielius.vl@gmail.com>
2021-06-21 20:15:44 +0300

    Changed Query from entity to only his id

--- BookShop.Core/Mediatr/Product/Queries/GetByAuthor/GetByAuthorProduct.cs
+++ BookShop.Core/Mediatr/Product/Queries/GetByAuthor/GetByAuthorProduct.cs
@@ -11,37 +11,36 @@ namespace BookShop.Core.Mediatr.Product.Queries.GetByAuthor
 {
     public static class GetByAuthorProduct
     {
-        public record Query(Domain.Entities.BookAuthor Author) : IRequest<IList<ProductModel>>;
+        public record Query(int AuthorId) : IRequest<IList<ProductModel>>;
 
         public class Handler : IRequestHandler<Query, IList<ProductModel>>
         {
             private readonly IProductRepository repository;
+            private readonly IBookAuthorRepository bookAuthorRepository;
             private readonly IMapper mapper;
 
-            public Handler(IProductRepository repository, IMapper mapper)
+            public Handler(IProductRepository repository, IBookAuthorRepository bookAuthorRepository, IMapper mapper)
             {
                 this.repository = repository;
+                this.bookAuthorRepository = bookAuthorRepository;
                 this.mapper = mapper;
             }
 
             public async Task<IList<ProductModel>> Handle(Query request, CancellationToken cancellationToken)
             {
-                if (repository == null)
-                {
-                    throw new ServiceNullException(nameof(IProductRepository), nameof(Handler));
-                }
-
-                if (mapper == null)
-                {
-                    throw new ServiceNullException(nameof(IMapper), nameof(Handler));
-                }
-
                 if (request == null)
                 {
                     throw new ValidationException("Query cannot be null");
                 }
 
-                IList<Domain.Entities.Product> products = repository.GetByAuthor(request.Author);
+                Domain.Entities.BookAuthor author = await bookAuthorRepository.GetById(request.AuthorId);
+
+                if(author == null)
+                {
+                    throw new NotFoundException(nameof(Domain.Entities.BookAuthor), author.Id);
+                }
+
+                IList<Domain.Entities.Product> products = repository.GetByAuthor(author);
 
                 if (products == null || products.Count == 0)
                 {
