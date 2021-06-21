480d902f99cc89cdfe9dd077e9a66c40e78e5a4c
Danielius <danielius.vl@gmail.com>
2021-06-21 19:06:29 +0300

    Added CategoryController

---
+++ BookShop.Api/Controllers/CategoryController.cs
@@ -0,0 +1,65 @@
+﻿using BookShop.Core.Mediatr.Category.Commands.Create;
+using BookShop.Core.Mediatr.Category.Commands.Delete;
+using BookShop.Core.Mediatr.Category.Commands.Update;
+using BookShop.Core.Mediatr.Category.Queries.GetAll;
+using BookShop.Core.Mediatr.Category.Queries.GetById;
+using BookShop.Core.Models;
+using BookShop.Identity.Authorization;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookShop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public CategoryController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            IList<CategoryModel> categories = await mediator.Send(new GetAllCategory.Query());
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            CategoryModel category = await mediator.Send(new GetByIdCategory.Query(id));
+            return Ok(category);
+        }
+
+        [HttpPost]
+        [Authorize(Policy = RoleConstants.ModeratorName)]
+        public async Task<IActionResult> Add([FromBody] CreateCategory.Command command)
+        {
+            await mediator.Send(command);
+            return Ok();
+        }
+
+        [HttpPut]
+        [Authorize(Policy = RoleConstants.ModeratorName)]
+        public async Task<IActionResult> Update([FromBody] UpdateCategory.Command command)
+        {
+            await mediator.Send(command);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Policy = RoleConstants.ModeratorName)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await mediator.Send(new DeleteCategory.Command(id));
+            return Ok();
+        }
+    }
+}
