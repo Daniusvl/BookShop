3789bdcf53680c3b74039522ca3c780504511785
Danielius <danielius.vl@gmail.com>
2021-06-21 20:28:41 +0300

    Finished ProductController

--- BookShop.Api/Controllers/ProductController.cs
+++ BookShop.Api/Controllers/ProductController.cs
@@ -2,7 +2,11 @@
 using BookShop.Core.Mediatr.Product.Commands.Delete;
 using BookShop.Core.Mediatr.Product.Commands.Update;
 using BookShop.Core.Mediatr.Product.Queries.GetAll;
+using BookShop.Core.Mediatr.Product.Queries.GetByAuthor;
+using BookShop.Core.Mediatr.Product.Queries.GetByCategory;
 using BookShop.Core.Mediatr.Product.Queries.GetById;
+using BookShop.Core.Mediatr.Product.Queries.GetByName;
+using BookShop.Core.Mediatr.Product.Queries.GetByPrice;
 using BookShop.Core.Models;
 using BookShop.Identity.Authorization;
 using MediatR;
@@ -36,6 +40,34 @@ namespace BookShop.Api.Controllers
         {
             ProductModel product = await mediator.Send(new GetByIdProduct.Query(id));
             return Ok(product);
+        }
+
+        [HttpGet("/ByAuthor/{id}")]
+        public async Task<IActionResult> GetByAuthor(int id)
+        {
+            IList<ProductModel> products = await mediator.Send(new GetByAuthorProduct.Query(id));
+            return Ok(products);
+        }
+
+        [HttpGet("/ByCategory/{id}")]
+        public async Task<IActionResult> GetByCategory(int id)
+        {
+            IList<ProductModel> products = await mediator.Send(new GetByCategoryProduct.Query(id));
+            return Ok(products);
+        }
+
+        [HttpGet("/ByName/{name}")]
+        public async Task<IActionResult> GetByName(string name)
+        {
+            IList<ProductModel> products = await mediator.Send(new GetByNameProduct.Query(name));
+            return Ok(products);
+        }
+
+        [HttpGet("/ByPrice/{Min}/{Max}")]
+        public async Task<IActionResult> GetByName(decimal min, decimal max)
+        {
+            IList<ProductModel> products = await mediator.Send(new GetByPriceProduct.Query(min, max));
+            return Ok(products);
         }
 
         [HttpPost]
