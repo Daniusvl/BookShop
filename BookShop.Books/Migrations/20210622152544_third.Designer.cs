5432eedc74156d976c69a925056910619bf89fad
Danielius <danielius.vl@gmail.com>
2021-06-22 18:27:45 +0300

    Applied third migration

--- BookShop.Books/Migrations/20210622152544_third.Designer.cs
+++ BookShop.Books/Migrations/20210622152544_third.Designer.cs
@@ -4,14 +4,16 @@ using BookShop.Books;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookShop.Books.Migrations
 {
     [DbContext(typeof(BooksDb))]
-    partial class BooksDbModelSnapshot : ModelSnapshot
+    [Migration("20210622152544_third")]
+    partial class third
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BookShop.Books/Migrations/20210622152544_third.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookShop.Books.Migrations
+{
+    public partial class third : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Books_BookId",
+                table: "Photos");
+
+            migrationBuilder.DropColumn(
+                name: "PhotoId",
+                table: "Photos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BookId",
+                table: "Photos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2021, 6, 22, 18, 25, 38, 899, DateTimeKind.Local).AddTicks(3703));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2021, 6, 22, 18, 25, 38, 903, DateTimeKind.Local).AddTicks(8729));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2021, 6, 22, 18, 25, 38, 903, DateTimeKind.Local).AddTicks(6418));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Books_BookId",
+                table: "Photos",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Books_BookId",
+                table: "Photos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BookId",
+                table: "Photos",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PhotoId",
+                table: "Photos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2021, 6, 22, 18, 15, 9, 13, DateTimeKind.Local).AddTicks(6679));
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2021, 6, 22, 18, 15, 9, 19, DateTimeKind.Local).AddTicks(1162));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2021, 6, 22, 18, 15, 9, 18, DateTimeKind.Local).AddTicks(8649));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Books_BookId",
+                table: "Photos",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
