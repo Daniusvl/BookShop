71c01d8f9a6e3e57cf6135da01d3ea528a0816b2
Danielius <danielius.vl@gmail.com>
2021-06-24 23:50:08 +0300

    Added AdminController with ChangeRole entry

---
+++ BookShop.Api/Controllers/AdminController.cs
@@ -0,0 +1,44 @@
+﻿using BookShop.Core;
+using BookShop.Core.Abstract.Identity;
+using BookShop.Core.Models.User;
+using BookShop.Identity.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BookShop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize(Policy = RoleConstants.AdministratorName)]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IUserService userService;
+
+        public AdminController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpPost("/ChangeRole")]
+        public async Task<ActionResult> ChangeRole([FromBody] ChangeRoleRequest request)
+        {
+            bool result = false;
+            switch (request.Role)
+            {
+                case "DefaultUser":
+                     result = await userService.ChangeRole(request.UserId, Role.DefaultUser);
+                    if (result)
+                        return Ok(result);
+                    else return BadRequest();
+                case "Manager":
+                    result = await userService.ChangeRole(request.UserId, Role.Manager);
+                    if (result)
+                        return Ok(result);
+                    else return BadRequest();
+                default:
+                    return BadRequest(false);
+            }
+        }
+    }
+}
