2ce70d897410002d3abee628ca1e771d634aa3ee
Danielius <danielius.vl@gmail.com>
2021-06-24 20:42:26 +0300

    Added Create and getById Category Test

--- BookShop.Books.Tests/BooksDbTests.cs
+++ BookShop.Books.Tests/BooksDbTests.cs
@@ -15,6 +15,7 @@ namespace BookShop.Books.Tests
         [Fact]
         public async Task Create_Author_Test()
         {
+            // arrange
             DbContextOptionsBuilder<BooksDb> options = new();
             options.UseInMemoryDatabase(MethodBase.GetCurrentMethod().Name);
 
@@ -31,10 +32,47 @@ namespace BookShop.Books.Tests
                 Name = "Jeffrey Richter"
             };
 
+            // act
             await repository.Create(author);
 
+            // assert
             Assert.True(author.Id != 0);
             Assert.True(author.CreatedBy == $"{nameof(BooksDbTests)} {MethodBase.GetCurrentMethod().Name}");
+        }
+
+        [Fact]
+        public async Task Create_And_GetById_Category_Test()
+        {
+            // arrange
+            DbContextOptionsBuilder<BooksDb> options = new();
+            options.UseInMemoryDatabase(MethodBase.GetCurrentMethod().Name);
+
+            Mock<ILoggedInUser> logged_in_user = new();
+            logged_in_user.SetupGet(ex => ex.UserId)
+                .Returns($"{nameof(BooksDbTests)} {MethodBase.GetCurrentMethod().Name}");
+
+            using BooksDb context = new TestableBooksDb(options.Options, logged_in_user.Object);
+
+            IAsyncRepository<Category> repository = new AsyncRepository<Category>(context);
+
+            Category category = new()
+            {
+                Name = "Technical"
+            };
+
+            // act
+            await repository.Create(category);
+
+            // assert
+            Assert.True(category.Id != 0);
+            Assert.True(category.CreatedBy == $"{nameof(BooksDbTests)} {MethodBase.GetCurrentMethod().Name}");
+
+            // act again
+            Category result = await repository.GetById(category.Id);
+
+            // assert again
+            Assert.NotNull(result);
+            Assert.Equal(category, result);
         }
     }
 }
