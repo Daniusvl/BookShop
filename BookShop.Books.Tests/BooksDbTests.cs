206dfb0a624673fc073f361355c216643cb69a12
Danielius <danielius.vl@gmail.com>
2021-06-24 18:55:56 +0300

    Added BooksDbTests class where integration db tests will be

---
+++ BookShop.Books.Tests/BooksDbTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.Books.Tests
+{
+    public class BooksDbTests
+    {
+        public async Task Create_Author_Test()
+        {
+            DbContextOptionsBuilder<BooksDb> options = new();
+            options.UseInMemoryDatabase(MethodBase.GetCurrentMethod().Name);
+
+            BooksDb context = new(options.Options, null, null);
+        }
+    }
+}
