9978a9f47c38ab2020496c9b9df29797235067b1
Danielius <danielius.vl@gmail.com>
2021-06-24 21:22:27 +0300

    Added ctor for unit test since in default ctor extension method of IConfiguration is used wich cannot be mocked

--- BookShop.Identity/IdentityDb.cs
+++ BookShop.Identity/IdentityDb.cs
@@ -8,6 +8,16 @@ namespace BookShop.Identity
 {
     public class IdentityDb : IdentityDbContext<AppUser>
     {
+        /// <summary>
+        /// !!!! USE ONLY FOR UNIT TESTING !!!!
+        /// </summary>
+        /// <param name="options"></param>
+        protected IdentityDb(DbContextOptions<IdentityDb> options) : base(options)
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
         public IdentityDb(DbContextOptions<IdentityDb> options, IConfiguration configuration) : base(options)
         {
             if (configuration.IsDevelopment())
