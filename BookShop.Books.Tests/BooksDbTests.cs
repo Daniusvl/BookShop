e24123fd4f19cd8d2fdbabc9cbfc36920108f4b4
Danielius <danielius.vl@gmail.com>
2021-06-24 20:32:04 +0300

    Added Create Author Test

--- BookShop.Books.Tests/BooksDbTests.cs
+++ BookShop.Books.Tests/BooksDbTests.cs
@@ -1,21 +1,40 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using BookShop.Books.Repositories;
+using BookShop.Core.Abstract;
+using BookShop.Core.Abstract.Repositories.Base;
+using BookShop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace BookShop.Books.Tests
 {
     public class BooksDbTests
     {
+        [Fact]
         public async Task Create_Author_Test()
         {
             DbContextOptionsBuilder<BooksDb> options = new();
             options.UseInMemoryDatabase(MethodBase.GetCurrentMethod().Name);
 
-            BooksDb context = new(options.Options, null, null);
+            Mock<ILoggedInUser> logged_in_user = new();
+            logged_in_user.SetupGet(ex => ex.UserId)
+                .Returns($"{nameof(BooksDbTests)} {MethodBase.GetCurrentMethod().Name}");
+
+            using BooksDb context = new TestableBooksDb(options.Options, logged_in_user.Object);
+
+            IAsyncRepository<Author> repository = new AsyncRepository<Author>(context);
+
+            Author author = new()
+            {
+                Name = "Jeffrey Richter"
+            };
+
+            await repository.Create(author);
+
+            Assert.True(author.Id != 0);
+            Assert.True(author.CreatedBy == $"{nameof(BooksDbTests)} {MethodBase.GetCurrentMethod().Name}");
         }
     }
 }
