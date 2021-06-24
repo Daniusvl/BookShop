61e0384ccbfcd65a369e303811b809903d0c60cd
Danielius <danielius.vl@gmail.com>
2021-06-24 21:33:51 +0300

    Revert

--- BookShop.Identity/IdentityDb.cs
+++ BookShop.Identity/IdentityDb.cs
@@ -8,16 +8,6 @@ namespace BookShop.Identity
 {
     public class IdentityDb : IdentityDbContext<AppUser>
     {
-        /// <summary>
-        /// !!!! USE ONLY FOR UNIT TESTING !!!!
-        /// </summary>
-        /// <param name="options"></param>
-        protected IdentityDb(DbContextOptions<IdentityDb> options) : base(options)
-        {
-            Database.EnsureDeleted();
-            Database.EnsureCreated();
-        }
-
         public IdentityDb(DbContextOptions<IdentityDb> options, IConfiguration configuration) : base(options)
         {
             if (configuration.IsDevelopment())
