6e7ae4130bc6d7bb7b24499323f2a0e50b3a4a9d
Danielius <danielius.vl@gmail.com>
2021-06-24 20:31:27 +0300

    Created new context for unit testing beacouse default context ctor uses extension method wich cannot be mocked

---
+++ BookShop.Books.Tests/TestableBooksDb.cs
@@ -0,0 +1,13 @@
+﻿using BookShop.Core.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShop.Books.Tests
+{
+    public class TestableBooksDb : BooksDb
+    {
+        public TestableBooksDb(DbContextOptions<BooksDb> options, ILoggedInUser loggedInUser)
+            : base(options, loggedInUser) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+    }
+}
