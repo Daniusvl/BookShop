ac20f4f8b5222e95d1685f36c0317c0dcb78ee5c
Danielius <danielius.vl@gmail.com>
2021-06-24 22:20:14 +0300

    Added Payment controller with BuyBook

---
+++ BookShop.Api/Controllers/PaymentController.cs
@@ -0,0 +1,30 @@
+﻿using BookShop.Core.Abstract;
+using BookShop.Core.Abstract.Identity;
+using BookShop.Core.Models.Payment;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BookShop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IAuthenticationService authService;
+        private readonly ILoggedInUser logged_in_user;
+
+        public PaymentController(IAuthenticationService authService, ILoggedInUser logged_in_user)
+        {
+            this.authService = authService;
+            this.logged_in_user = logged_in_user;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BuyBook([FromBody] BuyBookRequest buyBook)
+        {
+            bool result = await authService.AddOwnedProduct(logged_in_user.UserId, buyBook.BookId);
+
+            return Ok(result);
+        }
+    }
+}
