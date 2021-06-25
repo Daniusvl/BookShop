600d8201581415caed245b22f6266432d9ae2598
Danielius <danielius.vl@gmail.com>
2021-06-25 06:11:37 +0300

    Added ErrorResponse class

---
+++ BookShop.Core/Models/Error/ErrorResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace BookShop.Core.Models.Error
+{
+    public class ErrorResponse
+    {
+        public ErrorResponse(string exceptionName, string message)
+        {
+            ExceptionName = exceptionName;
+            Message = message;
+        }
+
+        public string ExceptionName { get; }
+        public string Message { get; }
+    }
+}
