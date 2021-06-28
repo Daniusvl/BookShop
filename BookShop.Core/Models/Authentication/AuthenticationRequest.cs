f5b25838d6ee8299361d107f3cae3113597894a1
Danielius <danielius.vl@gmail.com>
2021-06-28 23:13:57 +0300

    Added [Required] attribute to properties

--- BookShop.Core/Models/Authentication/AuthenticationRequest.cs
+++ BookShop.Core/Models/Authentication/AuthenticationRequest.cs
@@ -1,9 +1,13 @@
-﻿namespace BookShop.Core.Models.Authentication
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.Core.Models.Authentication
 {
     public class AuthenticationRequest
     {
+        [Required]
         public string Email { get; set; }
 
+        [Required]
         public string Password { get; set; }
     }
 }
