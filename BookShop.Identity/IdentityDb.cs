d2a354f3edbaf455b9e6df29daac128318b86c3a
Danielius <danielius.vl@gmail.com>
2021-06-29 19:30:52 +0300

    Added Admin to IdentityDb with password=Email.Password

--- BookShop.Identity/IdentityDb.cs
+++ BookShop.Identity/IdentityDb.cs
@@ -1,13 +1,56 @@
-﻿using BookShop.Identity.Models;
+﻿using BookShop.Features.Configuration;
+using BookShop.Identity.Authorization;
+using BookShop.Identity.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace BookShop.Identity
 {
     public class IdentityDb : IdentityDbContext<AppUser>
     {
-        public IdentityDb(DbContextOptions<IdentityDb> options) : base(options)
+        private readonly Email email;
+
+        public IdentityDb(DbContextOptions<IdentityDb> options, IOptions<Email> email) : base(options)
         {
+            this.email = email.Value;
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            string user_id = "1";
+            int claim_id = 999999999;
+
+            PasswordHasher<AppUser> hasher = new();
+
+            AppUser user = new()
+            {
+                Id = user_id,
+                Email = email.UserName,
+                NormalizedEmail = email.UserName.ToUpper(),
+                EmailConfirmed = true,
+                UserName = "Admin",
+                NormalizedUserName = "ADMIN",
+                SecurityStamp = string.Empty,
+                PasswordHash = hasher.HashPassword(null, email.Password)
+            };
+
+            builder.Entity<AppUser>()
+                .HasData(user);
+
+            IdentityUserClaim<string> claim = new()
+            {
+                Id = claim_id,
+                UserId = user_id,
+                ClaimType = RoleConstants.RoleClaim,
+                ClaimValue = RoleConstants.AdministratorName
+            };
+
+            builder.Entity<IdentityUserClaim<string>>()
+                .HasData(claim);
         }
     }
 }
