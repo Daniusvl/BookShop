fbe6238c70501102c2faf8c7caabab60cf50c071
Danielius <danielius.vl@gmail.com>
2021-07-01 18:23:45 +0300

    #5 created window factory

---
+++ BookShop.CRM/IWindowFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace BookShop.CRM
+{
+    public interface IWindowFactory
+    {
+        public TWindow CreateWindow<TWindow>()
+            where TWindow : Window;
+    }
+}
---
+++ BookShop.CRM/WindowFactory.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using System.Windows;
+
+namespace BookShop.CRM
+{
+    public class WindowFactory : IWindowFactory
+    {
+        public TWindow CreateWindow<TWindow>()
+            where TWindow : Window
+        {
+            return AutoFac.Container.Resolve<TWindow>();
+        }
+    }
+}
