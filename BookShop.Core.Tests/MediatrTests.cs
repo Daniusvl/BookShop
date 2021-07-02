66d71f996b2f9ef89293e1a4ac3b2884dc079031
Danielius <danielius.vl@gmail.com>
2021-07-02 23:45:23 +0300

    Removed file craetion test since its logic changed

--- BookShop.Core.Tests/MediatrTests.cs
+++ BookShop.Core.Tests/MediatrTests.cs
@@ -21,55 +21,6 @@ namespace BookShop.Core.Tests
     public class MediatrTests
     {
         [Fact]
-        public async Task Photo_File_Creation_Test()
-        {
-            // arange
-            Mock<IAsyncRepository<Photo>> base_photo_repository = new();
-
-            Mock<IPhotoRepository> photo_repository = new();
-            photo_repository.SetupGet(repo => repo.BaseRepository).Returns(base_photo_repository.Object);
-
-            Mock<IBookRepository> book_repository = new();
-            book_repository.Setup(ex => ex.ContainsWithName(It.IsAny<string>()))
-                .Returns(Task.FromResult(true));
-            book_repository.Setup(ex => ex.GetByName(It.IsAny<string>()))
-                .Returns(Task.FromResult(new Book { Id = 1 }));
-
-            Mock<IUnitOfWork> unit_of_work = new();
-            unit_of_work.SetupGet(ex => ex.BookRepository)
-                .Returns(book_repository.Object);
-            unit_of_work.SetupGet(ex => ex.PhotoRepository)
-                .Returns(photo_repository.Object);
-
-            Mock<ILogger<CreatePhotoCommandHandler>> logger = new();
-
-            Mock<IMapper> mapper = new Mock<IMapper>();
-            Func<Photo, PhotoModel> func = photo => new PhotoModel() { FileBytes = File.ReadAllBytes(photo.FilePath) };
-            mapper.Setup(ex => ex.Map<Photo, PhotoModel>(It.IsAny<Photo>()))
-                .Returns(func);
-
-            CreatePhotoCommand command = new() { ProductName = "ProductName", FileBytes = new List<byte> { 0x99, 0x99 } };
-            CreatePhotoCommandHandler handler = new(unit_of_work.Object, mapper.Object, logger.Object);
-
-            // act
-            PhotoModel result = await handler.Handle(command, default);
-
-            bool file_exists = result.FileBytes != null && result.FileBytes.Count > 0;
-
-            // delete created junk file and directories
-            foreach (FileInfo file_info in new DirectoryInfo(Directory.GetCurrentDirectory() + @"\Photos\ProductName").GetFiles())
-            {
-                file_info.Delete();
-            }
-            Directory.Delete($@"Photos\{command.ProductName}");
-            Directory.Delete("Photos");
-
-            // assert
-            Assert.NotNull(result);
-            Assert.True(file_exists);
-        }
-
-        [Fact]
         public async Task Update_Author_Throw_Exception_If_Id_Not_Found_Test()
         {
             // arange
