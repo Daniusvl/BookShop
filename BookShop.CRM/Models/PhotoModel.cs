4fa8c3e3fb1b364f66922e5896220cda4cd53407
Danielius <danielius.vl@gmail.com>
2021-07-03 03:52:25 +0300

    Removed FileBytes property

--- BookShop.CRM/Models/PhotoModel.cs
+++ BookShop.CRM/Models/PhotoModel.cs
@@ -1,12 +1,8 @@
-﻿using System.Collections.Generic;
-
-namespace BookShop.CRM.Models
+﻿namespace BookShop.CRM.Models
 {
     public class PhotoModel
     {
         public int Id { get; set; }
-
-        public IList<byte> FileBytes { get; set; }
 
         public int BookId { get; set; }
 
