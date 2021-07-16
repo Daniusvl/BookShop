2540bcf2f593f6d760ca6c01a644ea4363d26be0
Danielius <danielius.vl@gmail.com>
2021-07-16 06:01:32 +0300

    Removed books filter reducer (#7)

--- BookShop.BrowserClient/ClientApp/src/store.js
+++ BookShop.BrowserClient/ClientApp/src/store.js
@@ -1,9 +1,7 @@
 ﻿import { createStore, combineReducers } from 'redux';
-import BooksFilterReducer from './reducers/books_filter_reducer';
 import BooksListReducer from './reducers/books_list_reducer';
 
 const store = createStore(combineReducers({
-    BooksFilterReducer,
     BooksListReducer
 }));
 
